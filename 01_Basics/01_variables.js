8a6eaaa18e1f748e77945dc1442f4af79af21e1a
Diya Arora <145909573+diya05arora@users.noreply.github.com>
2024-03-15 18:11:04 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "diya@google.com"
+var accountPasswd = "12345"
+accountCity = "Jaipur" //not a good method
+let accountState;
+
+// accountId=2 //not allowed
+
+accountEmail="abc@gmail.com"
+accountPasswd="5639"
+accountCity="Delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var necause of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPasswd, accountCity, accountState]);
