e2a9c434eb4351d5ad7ab9f1700600f9cceb1a88
Diya Arora <145909573+diya05arora@users.noreply.github.com>
2024-03-16 04:18:27 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperators.js
@@ -0,0 +1,80 @@
+// let score = "23"
+
+// console.log(typeof score);
+// console.log(typeof(score)); //same
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// let score = "23abc"
+
+// console.log(typeof score);
+// console.log(typeof(score)); //same
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score)); //same
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score)); //same
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score)); //same
+
+// let valueInNumber = Number(score); //notice the capital N 
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//CONVERSION TO NUMBER
+// "23" => 23
+// "23abc" => NaN (Not a Number)
+// true => 1, false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// let isLoggedIn = "Diya";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// CONVERSION TO BOOLEAN
+// 1 => true
+// 0 => false
+// "" (empty string) => false
+// "xyz" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
