0aadf37043308d4319e3383bdfb23a167b800bf0
Diya Arora <145909573+diya05arora@users.noreply.github.com>
2024-03-17 09:52:38 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Hitesh";
+const repoCount = 50;
+// console.log(name , repoCount , "Value"); //old method
+
+//String interpolation
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`); //better syntax
+
+const gameName = new String("Hitesh-hc-com"); //one more way to declare strings
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName)
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4); //end value not included
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 7);
+console.log(anotherString);
+
+const newString1 = "    hitesh\n     " //trim removes \n too
+console.log(newString1);
+const newString2 = newString1.trim();
+console.log(newString2);
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("hitesh"));
+console.log(url.includes("Hitesh"));
+
+console.log(gameName.split("-"));
